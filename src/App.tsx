0f99c55d8fd39b40e95701c9ddedf92af14f0bc1
OtoBourbon <sharvashidze0712@gmail.com>
2025-02-28 06:28:05 +0400

    updated lazy loader

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import Loader from "./pages/Loader";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
   useEffect(() => {
     const img = new Image();
     img.src = "../usa-flag.jpg";
-    img.onload = () => setIsLoaded(true);
+    img.onload = () => {
+      setIsLoaded(true);
+    };
   }, []);
 
   return (
-    <div className="App">{isLoaded ? <LibertyToggler /> : <Loader />}</div>
+    <div
+      className="App"
+      style={{ backgroundImage: isLoaded ? 'url("../usa-flag.jpg")' : "none" }}>
+      {isLoaded ? <LibertyToggler /> : <Loader />}
+    </div>
   );
 }
 
